b18f5f2e3818c6206b1d5605de9a8007a862027b
muyi <15699851889@163.com>
2018-06-10 18:08:51 +0800

    four

--- src/main/java/com/baizhi/entity/produce.java
+++ src/main/java/com/baizhi/entity/produce.java
@@ -5,6 +5,15 @@ public class produce {
     private String name;
     private String password;
     private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public produce() {
     }
 
